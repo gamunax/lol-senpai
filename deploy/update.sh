b8fda5dad6270f0891a950e6edae19305684f93f
Maigret Aurélien <dewep.net@gmail.com>
2015-04-05 20:10:06 +0200

    update script

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+git pull
+
 docker stop lol-senpai
 docker rm -f lol-senpai
 docker rmi -f lol-senpai
-
-git pull
 
 docker build -t lol-senpai .
 docker run --name lol-senpai -p 5000:5000 -d lol-senpai
