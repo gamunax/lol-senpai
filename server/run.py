95d7e0142fe9a89671e4eaff0527b13a7b4b7397
Maigret Aurélien <dewep.net@gmail.com>
2015-04-05 18:27:12 +0200

    Server Flask

---
+++ server/run.py
@@ -0,0 +1,19 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    title = "Home"
+    return render_template('main.html', **locals())
+
+
+@app.route('/match/<username>')
+def match(username):
+    title = username + "'s match"
+    return render_template('match.html', **locals())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
