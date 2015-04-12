10734fb45fc0512439a7864ff9593bde8944d5cb
Maigret Aurélien <dewep.net@gmail.com>
2015-04-12 23:54:42 +0200

    Refonte du thème

--- server/static/script.js
+++ server/static/script.js
@@ -1,9 +1,13 @@
 (function(){
+
+    /* GENERAL */
+    var i, j;
     var page_lang = document.getElementsByTagName('html')[0].attributes["lang"].value;
 
+    /* LANG */
     var menu = document.getElementsByClassName("lang");
-    for (var i = 0; i < menu.length; i++){
-        for (var j = 0; j < menu[i].classList.length; j++){
+    for (i = 0; i < menu.length; i++){
+        for (j = 0; j < menu[i].classList.length; j++){
             if (menu[i].classList[j].substr(0, 5) == "lang-"){
                 var current_lang = menu[i].classList[j].substr(5);
                 if (current_lang == page_lang){
@@ -23,4 +27,19 @@
             }
         }
     }
+
+    /* FORM */
+    var forms = document.getElementsByClassName("form_summoner_search");
+    for (i = 0; i < forms.length; i++) {
+        forms[i].onsubmit = function(form){
+            return function() {
+                var select = form.querySelector("[name=summoner_region]");
+                var region = select.options[select.selectedIndex].value;
+                var name = encodeURIComponent(form.querySelector("[name=summoner_name]").value);
+                window.location.href = "/" + page_lang + "/game/" + region + "/" + name;
+                return false;
+            }
+        }(forms[i]);
+    }
+
 })();
