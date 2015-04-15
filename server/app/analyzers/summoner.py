466d908de0f7db4c479a8970c041a9ae65f5732f
Stego <alexandre.nucera@gmail.com>
2015-04-15 23:14:40 +0200

    bug fix (too many args)

--- server/app/analyzers/summoner.py
+++ server/app/analyzers/summoner.py
@@ -4,5 +4,5 @@ from app.data import get_info_summoner
 
 class AnalyzerSummoner(AnalyzerBase):
     def analyze_player_as_an_ally(self, senpai, player):
-        stats = get_info_summoner(player.id, senpai.game.gameQueue)
+        stats = get_info_summoner(player.id)
         print("LAAAAAAAA", stats)
