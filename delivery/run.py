a968dca3a9d33332a9b0e149432f326c42b6653c
Maigret Aurélien <dewep.net@gmail.com>
2015-04-19 23:30:39 +0200

    Delivery: Build new release + images for the index

--- delivery/run.py
+++ delivery/run.py
@@ -1,8 +1,9 @@
 __author__ = 'Dewep'
 
-from flask import render_template, Flask, request, Response
+from flask import render_template, Flask, request, Response, abort
 import os
 import subprocess
+from docker import Client
 
 app = Flask(__name__)
 root_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -27,24 +28,30 @@ def get_latest_git_revision():
 
 @app.route('/')
 def root():
+    import time
     latest_git_revision = get_latest_git_revision()
+    c = Client()
     releases = list()
     containers = list()
-    is_used = False  # si il y a des containers qui
-    state = "danger"
-    if "27c3a0c" == latest_git_revision:
-        state = "success"
-    elif is_used:
-        state = "warning"
-    releases.append({
-        "image_id": "64698ee0dfe3",
-        "repository": "senpai",
-        "tag": "27c3a0c" + (" (latest)" if "27c3a0c" == latest_git_revision else ""),
-        "created": "2 hours ago",
-        "size": "786.6 MB",
-        "is_used": is_used,
-        "state": state
-    })
+    for image in c.images():
+        repo = None
+        tag = None
+        for repo_tag in image["RepoTags"]:
+            _repo, _tag = repo_tag.split(":")
+            if _repo == "senpai" and _tag != "latest":
+                repo = _repo
+                tag = _tag
+        if repo and tag:
+            release = dict()
+            release["image_id"] = image["Id"][:12]
+            release["repository"] = repo
+            release["tag"] = tag + (" (latest)" if tag == latest_git_revision else "")
+            release["created"] = time.ctime(image["Created"])
+            release["size"] = "%.1f MB" % (image["VirtualSize"] / 1000000.0)
+            release["is_used"] = True  # TODO
+            release["state"] = "success"  # TODO
+            releases.append(release)
+    # TODO: containers
     state = "success"
     has_image = False
     containers.append({
@@ -66,6 +73,26 @@ def execute_git_fetch():
     if request.headers.get('accept') == 'text/event-stream':
         return Response(execute_command(args), content_type='text/event-stream')
     return render_template('execute.html', path=request.path, command=args_to_command(args))
+
+
+@app.route('/execute/release/new/<revision>')
+def execute_release_new(revision):
+    args = ["git", "-C", root_directory, "checkout", revision]
+    p = subprocess.Popen(args, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return_code = int(str(p.wait()))
+    if return_code:
+        abort(404, "Revision not found")
+    if request.headers.get('accept') == 'text/event-stream':
+        def build():
+            import json
+            c = Client()
+            for line in c.build(path=root_directory, rm=True, tag='senpai:'+revision):
+                obj = json.loads(line.decode("utf-8"))
+                if "stream" in obj:
+                    yield "data: %s\n\n" % obj["stream"]
+            yield "data: RETURN_VALUE=0\n\n"
+        return Response(build(), content_type='text/event-stream')
+    return render_template('execute.html', path=request.path, command="docker build senpai:"+revision)
 
 
 if __name__ == '__main__':
